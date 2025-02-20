f0b9c499dce499d9bb44c156557a375c0da66931
Raheenfatima068 <Fatima.shahid.pakistan@gmail.com>
2025-02-20 09:03:42 +0500

    Added grocery List.cs with grocery items

---
+++ grocery List.cs
@@ -0,0 +1,18 @@
+class Program
+{
+  static void Main()
+  {
+    List<string> groceryItems = new List<string>
+    {
+      "Milk"
+        "Bread"
+        "Eggs"
+        "Butter"
+        "Rice"
+      };
+    foreach (var item in groceryitems)
+    {
+      Console.WriteLine(item);
+    }
+  }
+}
